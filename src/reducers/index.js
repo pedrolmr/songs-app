9c52a9ed31a73911525a32395c71bc89cf5adf23
pedro <pedroluismontesinosrivera@gmail.com>
2018-12-01 20:34:53 -0500

    created songsReducer reducer that displays every song in the app

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+const songsReducer = () => {
+    return [
+        { title: 'gummo', duration: '4:05' },
+        { title: 'bodak yellow', duration: '4:05' },
+        { title: 'love scars', duration: '4:05' }
+    ];
+};
